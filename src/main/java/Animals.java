2b241eb4a4916344de1afcbeb6db8053f7ed7721
eric <eric.kingoo2014@gmail.com>
2019-04-30 11:42:22 +0300

    add constructor -class Animals

--- src/main/java/Animals.java
+++ src/main/java/Animals.java
@@ -2,16 +2,24 @@ import org.sql2o.*;
 import java.util.ArrayList;
 import java.util.List;
 
-public abstract class Animals {
-    public String name;
-    public int id;
+public  class Animals {
+    private String endangered;
+    private String animal;
+    private String health;
+    private String age;
+    private int id;
 
+    public Animals(String animal, String endangered, String health, String age) {
+        this.animal = animal;
+        this.endangered = endangered;
+        this.health = health;
+        this.age = age;
+    }
 
 
     public int getId() {
         return id;
     }
-
 
 
     public String getName() {
@@ -27,12 +35,12 @@ public abstract class Animals {
 //
 //    }
 
-    public void save(){
+    public void save() {
 
-        try(Connection con = DB.sql2o.open() ){
-            String sql = "INSERT INTO animals(name,health,age) VALUES(:name,:health,:age)" ;
-            this.id=(int) con.createQuery(sql,true)
-                    .addParameter("name",this.name)
+        try (Connection con = DB.sql2o.open()) {
+            String sql = "INSERT INTO animals(name,health,age) VALUES(:name,:health,:age)";
+            this.id = (int) con.createQuery(sql, true)
+                    .addParameter("name", this.name)
                     .executeUpdate()
                     .getKey();
 
@@ -40,28 +48,28 @@ public abstract class Animals {
 
     }
 
-    public static List<Animals> all(){
+    public static List<Animals> all() {
         String sql = "SELECT * FROM animals";
-        try(Connection con =DB.sql2o.open()){
+        try (Connection con = DB.sql2o.open()) {
             return con.createQuery(sql)
                     .executeAndFetch(Animals.class);
         }
     }
 
-    public static Animals find(int id){
-        try(Connection con = DB.sql2o.open()){
+    public static Animals find(int id) {
+        try (Connection con = DB.sql2o.open()) {
             String sql = "SELECT * FROM animals WHERE id= :id";
             Animals animal = con.createQuery(sql)
                     .addParameter("id", id)
                     .executeAndFetchFirst(Animals.class);
-                    return animal;
+            return animal;
         }
     }
 
-
-
-
-
-
-
 }
+
+
+
+
+
+
